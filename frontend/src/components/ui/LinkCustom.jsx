e4271ca2afd5e992ff5b15b3c2e73159c4690d5e
VladKhurs <vladislavkhurs@gmail.com>
2026-01-31 14:22:51 +0300

    feat: template

--- frontend/src/components/ui/LinkCustom.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function LinkCustom({ to = "/", children, className=""}) {
-    return (
-        <Link to={to} className={`text-white hover:bg-theme-hover font-semibold text-lg h-full items-center px-5 flex ${className}`}>
-            {children}
-        </Link>
-    );
-}
